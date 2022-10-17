dad4580fd04569e1c5471cd6d891e55393be7db6
allantomas <32474234+RichardTomas@users.noreply.github.com>
2022-10-17 03:18:15 -0700

    First Commit

---
+++ M2-Challenge-Tomas-Richard-Allan/src/main/java/com/example/m2challengetomasrichardallan/exception/ArgumentNotValidException.java
@@ -0,0 +1,11 @@
+package com.example.m2challengetomasrichardallan.exception;
+
+public class ArgumentNotValidException extends RuntimeException{
+    public ArgumentNotValidException() {
+        super();
+    }
+
+    public ArgumentNotValidException(String message) {
+        super();
+    }
+}
